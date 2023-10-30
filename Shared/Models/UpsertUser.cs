135ff8fb2205f51db217cca2e639269a5165c162
a.gubala <a.gubala@artexit.pl>
2023-10-30 08:23:58 +0100

    Add max length for field FirstName and LastName in edit and add mode.

--- Shared/Models/UpsertUser.cs
+++ Shared/Models/UpsertUser.cs
@@ -1,9 +1,13 @@
-﻿namespace UserSpying.Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserSpying.Shared.Models
 {
     public record UpsertUser
     {
         public int GenderId { get; set; }
+        [MaxLength(50, ErrorMessage = "Maksymalna długość pola Imię wynosi 50 znaków")]
         public string FirstName { get; set; }
+        [MaxLength(150, ErrorMessage = "Maksymalna długość pola Imię wynosi 150 znaków")]
         public string LastName { get; set; }
         public DateTime? DateOfBirth { get; set; }
     }
